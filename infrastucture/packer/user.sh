b71be914529672e2179687aa45169020f710ace4
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-02-21 18:02:33 -0500

    creating the image for user

--- infrastucture/packer/user.sh
+++ infrastucture/packer/user.sh
@@ -2,4 +2,4 @@
 sudo groupadd csye6225
 sudo useradd -g csye6225 -s /usr/sbin/nologin csye6225
 sudo chown  -R csye6225:csye6225 /home/
-sudo chmod  744 /home/karthikvanabhojanacloud/*
+#sudo chmod  744 /home/karthikvanabhojanacloud/*
