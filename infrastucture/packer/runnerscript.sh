8d6670f3052f3d5a3eef19531908fe22dce6156f
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-02-21 16:46:36 -0500

    updating the code to fix the user

--- infrastucture/packer/runnerscript.sh
+++ infrastucture/packer/runnerscript.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo yum install -y unzip
 
 sudo yum install -y java-11-openjdk-devel
@@ -12,22 +13,17 @@ sudo dnf install -y maven
 #installing mysql
 
 
-#!/bin/bash
 
 sudo yum -y install mysql-server
 
-# Start the MySQL service
 sudo systemctl start mysqld
 
-# Wait for a second to ensure MySQL service is started
 sleep 1s
 
 echo "Installing SQL Server..."
 sudo yum install -y mysql-server
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
-
-
 
 
 
@@ -45,12 +41,12 @@ sudo groupadd csye6225
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
 sudo mkdir -p /opt/csye6225/
 
-sudo cp /tmp/application.jar /opt/csye6225/
-sudo chown -R csye6225:csye6225 /opt/csye6225/application.jar
+sudo cp /tmp/webapp-1.2.0-RELEASE.zip /opt/csye6225/
+sudo chown -R csye6225:csye6225 /opt/csye6225/target/webapp-1.2.0-RELEASE.jar
 
 
-sudo cp /tmp/application.jar /opt/csye6225/
-sudo chown -R csye6225:csye6225 /opt/csye6225/application.jar
+sudo cp /tmp/webapp-1.2.0-RELEASE.jar /opt/csye6225/
+sudo chown -R csye6225:csye6225 /opt/csye6225/target/webapp-1.2.0-RELEASE.jar
 
 cat <<EOF | sudo tee /etc/systemd/system/webapp.service
 [Unit]
@@ -60,7 +56,7 @@ After=network.target
 [Service]
 User=csye6225
 Group=csye6225
-ExecStart=/usr/bin/java -jar /opt/csye6225/application.jar
+ExecStart=/usr/bin/java -jar /opt/csye6225/target/webapp-1.2.0-RELEASE.jar
 WorkingDirectory=/opt/csye6225
 Restart=always
 
@@ -69,14 +65,10 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 
-# Reload systemd to recognize the new service
 sudo systemctl daemon-reload
 
-# Enable your service to start at boot
 sudo systemctl enable webapp
 
-# Optional: Start the service now
 sudo systemctl start webapp
 
-# Check the status of your service (optional)
 sudo systemctl status webapp
