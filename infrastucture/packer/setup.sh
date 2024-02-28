b8e970fe624989067c37f2feb0ec8954a845a4a7
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-02-28 00:01:53 -0500

    made the changes to correct the error in the packer format

--- infrastucture/packer/setup.sh
+++ infrastucture/packer/setup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 echo "Installing ..."
-
-
-
-echo "Installing Maven..."
-sudo dnf module enable  -y maven:3.8
-
-sudo dnf install -y maven
-
 echo "Installing Java..."
 sudo yum install -y java-11-openjdk-devel
 
@@ -16,6 +8,10 @@ sudo update-alternatives --set java $(sudo update-alternatives --list java | gre
 
 sudo update-alternatives --set javac $(sudo update-alternatives --list javac | grep 'java-11-openjdk' | head -n 1)
 
+echo "Installing Maven..."
+sudo dnf module enable  -y maven:3.8
+
+sudo dnf install -y maven
 
 echo "Installing unzip..."
 sudo yum install -y unzip
