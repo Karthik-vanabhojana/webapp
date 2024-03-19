fa8b5a991c043ac29013d8861a38a2991ec33cec
Karthik Vanabhojana <vanabhojana.k@northeastern.edu>
2024-03-18 23:46:23 -0400

    commiting the config file for log

--- infrastucture/packer/runservice.sh
+++ infrastucture/packer/runservice.sh
@@ -5,3 +5,6 @@ sudo setenforce 0
 sudo mv /tmp/start.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable start.service
+
+sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent/
+sudo systemctl restart google-cloud-ops-agent
